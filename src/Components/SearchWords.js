f2be68609e17b47ce05bdba72319ee7a2e354dd3
= <khushboosolanki2000@gmail.com>
2021-08-28 09:30:09 +0530

    installed react-icons package

--- src/Components/SearchWords.js
+++ src/Components/SearchWords.js
@@ -1,11 +1,15 @@
 import React, { useState } from 'react'
 import SWords from './Stylings/SWords.css'
 function SearchWords() {
+    const [data,setdata] = useState("")
     const [searchWord, setSearchWord] = useState('')
     return (
         <div className="wordContainer">
             <div className="inputClass">
-                <input name="searchWord" type="text" />
+                <input name="searchWord" type="text" placeholder="Search here....." onChange={(e) => { setSearchWord(e.target.value) }} />
+            </div>
+            <div>
+                <button >Search</button>
             </div>
         </div>
     )
