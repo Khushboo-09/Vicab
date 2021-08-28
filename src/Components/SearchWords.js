f689fea8f00d394b1d26692a5016dd84162c7546
= <khushboosolanki2000@gmail.com>
2021-08-28 09:36:19 +0530

    added getMeaning function to extract the data from API

--- src/Components/SearchWords.js
+++ src/Components/SearchWords.js
@@ -3,13 +3,23 @@ import SWords from './Stylings/SWords.css'
 function SearchWords() {
     const [data,setdata] = useState("")
     const [searchWord, setSearchWord] = useState('')
+
+    getMeaning = ()=>{
+        axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en_US/${searchWord}`)
+        .then((response)=>{
+            setdata(response.data[0]);
+        })
+        .catch((error)=>{
+            console.log(error)
+        })
+    }
     return (
         <div className="wordContainer">
             <div className="inputClass">
                 <input name="searchWord" type="text" placeholder="Search here....." onChange={(e) => { setSearchWord(e.target.value) }} />
             </div>
             <div>
-                <button >Search</button>
+                <button onClick = {getMeaning}>Search</button>
             </div>
         </div>
     )
